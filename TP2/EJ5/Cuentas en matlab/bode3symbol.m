d5706541e1a69c7baf0f5ce260d3ebffbd23ddef
jmestanza <jmestanza@itba.edu.ar>
2018-08-26 16:43:08 -0300

    added

--- TP2/EJ5/Cuentas en matlab/bode3symbol.m
+++ TP2/EJ5/Cuentas en matlab/bode3symbol.m
@@ -1,3 +1,6 @@
+clear all;
+close all;
+clc;
 syms C3;
 syms R5;
 syms R6;
@@ -5,8 +8,7 @@ syms C4;
 syms R7;
 syms s;
  
-ZB= R6+R5 + (1/(s*C3));
-ZAUX= (1/(s*C4));
-ZC = (R7*ZAUX)/(R7+ZAUX);
-H= ZC/(ZB+ZC);
-pretty(simplify(H))
+ZB = ((R5+R6)*s*C3 + 1)/(s*C3);
+ZC =  R7/(s*C4*R7+1);
+
+ZD=ZC/(ZB+ZC);
