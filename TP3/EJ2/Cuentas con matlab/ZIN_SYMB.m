b564678f3928e83ed2e972516653f527b9ed7cae
jmestanza <jmestanza@itba.edu.ar>
2018-09-17 23:29:16 -0300

    sigo conel informe

--- TP3/EJ2/Cuentas con matlab/ZIN_SYMB.m
+++ TP3/EJ2/Cuentas con matlab/ZIN_SYMB.m
@@ -14,6 +14,6 @@ Iin = (Vmas / Zg) + (Vin - Vmenos)/Rg;
 Zin = Vin/(Iin);
 %obtengo expresion
 Zin = getsfactor(Zin)
-
+%(Cg*Rg*Zg*s^2 + (Rg + BWP*Cg*Rg*Zg)*s + BWP*Rg)/((Cg*Rg + Cg*Zg)*s^2 + (BWP*Cg*Rg + 1)*s + BWP)
 
 
