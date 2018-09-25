15ee174c00332b6c789c9f929713c6f949354669
newtonis <newtonis.penguin@gmail.com>
2018-09-25 11:54:22 -0700

    add

--- TP3/EJ4/graficos.py
+++ TP3/EJ4/graficos.py
@@ -75,12 +75,12 @@ def plot_contraste(mode, filename):
 # plot_contraste("pha", "distintos_k_pha.png")
 
 
-def plot_mediciones(mode, mediciones_filename, spice_filename, output_filename, kvalue):
+def plot_mediciones(mode, mediciones_filename, spice_filename, output_filename, kvalue, tf=transferencias.get_transfer):
 
     fig, ax1 = plt.subplots()
     patches = []
 
-    t = transferencias.get_transfer(kvalue)
+    t = tf(kvalue)
     h = signal.lti(t[0], t[1])
 
     w, mag, pha = signal.bode(h, w_range)
@@ -119,11 +119,56 @@ def plot_mediciones(mode, mediciones_filename, spice_filename, output_filename, 
     datacursor_easy.make_datacursor(mode, "output/" + output_filename, plt, fig)
 
 
-plot_mediciones(mode="mag",
-                mediciones_filename="Ej4_Bode_k05.xlsx",
-                spice_filename="Bode_K05.txt",
-                output_filename="contraste_k=05_mag.png",
-                kvalue=0.5)
+def plot_mediciones_zin(mode, mediciones_filename, spice_filename, output_filename, kvalue, tf):
+    fig, ax1 = plt.subplots()
+    patches = []
+
+    t = tf(kvalue)
+    h = signal.lti(t[0], t[1])
+
+    w, H = signal.freqresp(h, w_range)
+    f = [i / 2 / pi for i in w]
+
+    if mode == "mag":
+        ax1.semilogx(f, abs(H), "green")
+    else:
+        angle = [atan2(H[i].imag, H[i].real)/pi*180.0 for i in range(len(H))]
+        ax1.semilogx(f, angle, "green")
+
+    patches.append(mpatches.Patch(color="green", label="Teórico"))
+
+    data_spice = read_spice.read_file_spice("Data de Simulacion para graficos/"+spice_filename)
+
+    if mode == "mag":
+        ax1.semilogx(data_spice["f"], [10**(i/20.0) for i in data_spice["abs"]], "red")
+    else:
+        ax1.semilogx(data_spice["f"], data_spice["pha"], "red")
+
+    patches.append(mpatches.Patch(color="red", label="Simulación"))
+
+    data_medido = read_xls.read_excel_data("Mediciones/"+mediciones_filename)
+
+    val = [data_medido["ZinR"][i] + data_medido["ZinIm"][i]*1j for i in range(len(data_medido["ZinR"]))]
+    abs_val = [abs(i) for i in val]
+
+    if mode == "mag":
+        ax1.semilogx(data_medido["Freq"], abs_val, "blue")
+    else:
+        ax1.semilogx(data_medido["Freq"], data_medido["Zfase"], "blue")
+
+    patches.append(mpatches.Patch(color="blue", label="Medido"))
+
+    plt.legend(handles=patches)
+
+    datacursor_easy.make_datacursor_zin(mode, "output/" + output_filename, plt, fig, plt)
+
+
+#
+# plot_mediciones(mode="mag",
+#                 mediciones_filename="Ej4_Bode_k05.xlsx",
+#                 spice_filename="Bode_K05.txt",
+#                 output_filename="contraste_k=05_mag.png",
+#                 kvalue=0.5)
 
 # plot_mediciones(mode="mag",
 #                 mediciones_filename="Ej4_Bode_k0.xlsx",
@@ -151,8 +196,32 @@ plot_mediciones(mode="mag",
 #                 kvalue=0)
 #
 #
-# plot_mediciones(mode="pha",
-#                 mediciones_filename="Ej4_Bode_k1.xlsx",
-#                 spice_filename="Bode_K1.txt",
-#                 output_filename="contraste_k=1_pha.png",
-#                 kvalue=1)
+
+plot_mediciones_zin(mode="mag",
+                mediciones_filename="Ej4_Bode_k1.xlsx",
+                spice_filename="Zin_K1.txt",
+                output_filename="contraste_zin_k=1_mag.png",
+                kvalue=1,
+                tf=transferencias.get_transfer_zin)
+
+plot_mediciones_zin(mode="pha",
+                mediciones_filename="Ej4_Bode_k1.xlsx",
+                spice_filename="Zin_K1.txt",
+                output_filename="contraste_zin_k=1_pha.png",
+                kvalue=1,
+                tf=transferencias.get_transfer_zin)
+
+plot_mediciones_zin(mode="mag",
+                mediciones_filename="Ej4_Bode_k0.xlsx",
+                spice_filename="Zin_K1.txt",
+                output_filename="contraste_zin_k=0_mag.png",
+                kvalue=1,
+                tf=transferencias.get_transfer_zin)
+
+plot_mediciones_zin(mode="pha",
+                mediciones_filename="Ej4_Bode_k0.xlsx",
+                spice_filename="Zin_K0.txt",
+                output_filename="contraste_zin_k=0_pha.png",
+                kvalue=1,
+                tf=transferencias.get_transfer_zin)
+
