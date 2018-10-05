d6783509aa8362b2df39ff62527e3af62136ee81
newtonis <newtonis.penguin@gmail.com>
2018-10-05 20:31:21 -0300

    add

--- TP4/proyecto_oficial/aprox/butter.py
+++ TP4/proyecto_oficial/aprox/butter.py
@@ -5,7 +5,7 @@ class Butter(Aprox):
     def __init__(self):
         pass
 
-    def configure(self, fp, fa, n=-1, ap=-1, aa=-1):
+    def configure(self, fp= -1, fa= -1, n=-1, ap=-1, aa=-1):
         pass
 
     def computar(self, freq_range, n=-1):
