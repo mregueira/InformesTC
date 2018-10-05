d6039be42ecd9136dd657881720043f89ab0727d
jmestanza <jmestanza@itba.edu.ar>
2018-10-05 19:57:59 -0300

    added butter to oficial project

--- TP4/proyecto_oficial/aprox/__init__.py
+++ TP4/proyecto_oficial/aprox/__init__.py
@@ -1,1 +1,3 @@
 # En este modulo estarán programadas todas las aproximaciones
+from aprox import *
+from butter import *
--- TP4/proyecto_oficial/aprox/aprox.py
+++ TP4/proyecto_oficial/aprox/aprox.py
@@ -2,7 +2,9 @@
 
 class Aprox:
     def __init__(self):
-        pass
+        self.f = None
+        self.mag = None
+        self.phase = None
 
     def computar(self, freq_range, n=-1):
         pass
