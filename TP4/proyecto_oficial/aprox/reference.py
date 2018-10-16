e594f30d6a170ee6949c35b81cf379f799bf9709
jmestanza <jmestanza@itba.edu.ar>
2018-10-16 00:19:23 -0300

    añadi cauer

--- TP4/proyecto_oficial/aprox/reference.py
+++ TP4/proyecto_oficial/aprox/reference.py
@@ -3,12 +3,13 @@
 from aprox.butter import Butter
 from aprox.cheby import Cheby
 from aprox.chebyinv import ChebyInv
+from aprox.cauer import Cauer
 
 mag_aprox = \
     {"Butterworth": Butter,
     "Chebycheff": Cheby,
     "Chebycheff inverso": ChebyInv,
-    "Cauer": None,
+    "Cauer": Cauer,
     "Legendre": None}
 
 
