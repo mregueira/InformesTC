9711ae1e22653c114d199e129b6da88cfcb4540b
newtonis <newtonis.penguin@gmail.com>
2018-10-29 21:47:41 -0300

    add

--- TP5/histogramas/computar_maximos.py
+++ TP5/histogramas/computar_maximos.py
@@ -17,4 +17,34 @@ def computar_maximos_bp(data):
         elif f2 == -1 and data["mag"][i] < max_value - margin:
             f2 = data["f"][i]
 
-    return {"max":max_value,"f1":f1,"f2":f2}
+    return {"max":max_value, "f1": f1, "f2": f2}
+
+
+def computar_notch(data):
+    min_value = 1e5
+    notch_f = -1
+    for i in range(len(data["mag"])):
+        if data["mag"][i] < min_value:
+            notch_f = data["f"][i]
+        min_value = min(min_value, data["mag"][i])
+
+    f1 = -1
+    f2 = -1
+    for i in range(len(data["mag"])):
+        if f1 == -1 and data["mag"][i] < -margin:
+            f1 = data["f"][i]
+        elif f2 == -1 and data["mag"][i] > -margin:
+            f2 = data["f"][i]
+    return {"notch_f": notch_f, "f1": f1, "f2": f2, "min": min_value}
+
+
+
+
+
+
+def conseguir_fp(data, cuttoff):
+    for i in range(len(data["abs"])):
+        if data["abs"][i] < cuttoff:
+            return (data["f"][i] + data["f"][i-1]) / 2
+    return -1
+
--- TP5/histogramas/read_spice_montecarlo.py
+++ TP5/histogramas/read_spice_montecarlo.py
@@ -28,43 +28,49 @@ def not_num(content):
 def read_file_spice(filename):
     file = open(filename,'r')
     lines = file.readlines()
-
-    data = dict()
-
-    data["f"] = []
-    data["abs"] = []
-    data["pha"] = []
     #print(lines)
+    #return 0
+    total_data = []
+    target_str = "Step Information"
+    i = 2
+    while i < len(lines):
+        data = {"f":[], "abs":[], "pha": []}
 
-    for i in range(1,len(lines)):
-        pnt = 0
-        c1 = ""
-        c2 = ""
-        c3 = ""
-        while lines[i][pnt] != '\t':
-            c1 += lines[i][pnt]
+        while i < len(lines) and (len(lines[i]) < len(target_str) or lines[i][:len(target_str)] != target_str):
+            pnt = 0
+            c1 = ""
+            c2 = ""
+            c3 = ""
+            while lines[i][pnt] != '\t':
+                c1 += lines[i][pnt]
+                pnt += 1
+
+            while not_num(lines[i][pnt]):
+                pnt += 1
+
+            while lines[i][pnt] != 'd':
+                c2 += lines[i][pnt]
+                pnt += 1
             pnt += 1
+            while not_num(lines[i][pnt]):
+                pnt += 1
+            while lines[i][pnt] != '°':
+                c3 += lines[i][pnt]
+                pnt += 1
 
-        while not_num(lines[i][pnt]):
-            pnt += 1
+            c1 = float(c1)
+            c2 = float(c2)
+            c3 = float(c3)
 
-        while lines[i][pnt] != 'd':
-            c2 += lines[i][pnt]
-            pnt += 1
-        pnt += 1
-        while not_num(lines[i][pnt]):
-            pnt += 1
-        while lines[i][pnt] != '°':
-            c3 += lines[i][pnt]
-            pnt += 1
+            data["f"].append(c1)
+            data["abs"].append(c2)
+            data["pha"].append(c3)
+            i += 1
+        total_data.append(data)
+        i += 1
+    return total_data
 
-        c1 = float(c1)
-        c2 = float(c2)
-        c3 = float(c3)
 
-        data["f"].append(c1)
-        data["abs"].append(c2)
-        data["pha"].append(c3)
-
-    return data
+#data = read_file_spice("EJ1/Circuito con Legendre/Simulacion/BodeMontecarlo.txt")
+#print(len(data))
 
