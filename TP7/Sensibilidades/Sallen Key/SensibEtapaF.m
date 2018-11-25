ad694668b8dfd4acc80cfbc1f68544cd5770f610
mmaspero <mmaspero@itba.edu.ar>
2018-11-25 19:39:11 -0300

    informe

--- TP7/Sensibilidades/Sallen Key/SensibEtapaF.m
+++ TP7/Sensibilidades/Sallen Key/SensibEtapaF.m
@@ -8,10 +8,12 @@ C1= 2.2e-9;
 C2 = C1;
 
 SQR1 = (C1*R2*R3 + C2*R1*R4 + C2*R2*R3)/(2*C1*R2*R3 - 2*C2*R1*R4 + 2*C2*R2*R3);
-SQR2 = -(C1*R2*R3 + C2*R1*R4 + C2*R2*R3)/(2*C1*R2*R3 - 2*C2*R1*R4 + 2*C2*R2*R3);
+SQR2 =-(C1*R2*R3 + C2*R1*R4 + C2*R2*R3)/(2*C1*R2*R3 - 2*C2*R1*R4 + 2*C2*R2*R3);
 SQR3 = -(C2*R1*R4)/(C1*R2*R3 - C2*R1*R4 + C2*R2*R3);
 SQR4 = (C2*R1*R4)/(C1*R2*R3 - C2*R1*R4 + C2*R2*R3);
-SQC1 = -(C1*R2*R3)/(C1*R2*R3 - C2*R1*R4 + C2*R2*R3);
+SQC1 = -(C1*R2*R3 + C2*R1*R4 - C2*R2*R3)/(2*C1*R2*R3 - 2*C2*R1*R4 + 2*C2*R2*R3);
 SQC2 = (C1*R2*R3 + C2*R1*R4 - C2*R2*R3)/(2*C1*R2*R3 - 2*C2*R1*R4 + 2*C2*R2*R3);
+
+
 
 fprintf('%f \n%f \n%f \n%f \n%f \n%f \n',SQR1,SQR2,SQR3,SQR4,SQC1,SQC2);
